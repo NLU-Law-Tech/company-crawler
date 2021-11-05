edd8c29648f800118a92b26f7ea17b31b3a7b1d8
p208p2002 <p208p2002@gmail.com>
2021-11-05 15:44:38 +0800

    Enable deccompress data

--- process_com.py
+++ process_com.py
@@ -45,17 +45,17 @@ def to_date_string(date_obj):
     return '/'.join(date_obj)
 
 if __name__ == "__main__":
-    # # deccompress data
-    # os.makedirs('./company_data/decompress',exist_ok=True)
+    # deccompress data
+    os.makedirs('./company_data/decompress',exist_ok=True)
     save_path = './company_data/decompress'
-    # files = glob.glob("./company_data/*.jsonl.gz")
-    # for file in files:
-    #     f_name = os.path.basename(file)
-    #     new_f_name = f_name[:-3]
-    #     decompress_file = os.path.join(save_path,new_f_name)
-    #     logger.info(f"{file} -> {decompress_file}")
-    #     decompress(infile=file,tofile=decompress_file)
-    #
+    files = glob.glob("./company_data/*.jsonl.gz")
+    for file in files:
+        f_name = os.path.basename(file)
+        new_f_name = f_name[:-3]
+        decompress_file = os.path.join(save_path,new_f_name)
+        logger.info(f"{file} -> {decompress_file}")
+        decompress(infile=file,tofile=decompress_file)
+    
 
     csv_file = open('company_list.csv', 'w', newline='')
     fieldnames = [
